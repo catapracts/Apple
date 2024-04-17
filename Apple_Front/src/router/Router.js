7c11384ff6f088140c79d34b23aec8a292f1b70d
waji <151129358+wagji@users.noreply.github.com>
2024-04-17 12:25:52 +0900

    test

--- Apple_Front/src/router/Router.js
+++ Apple_Front/src/router/Router.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../app/Home"
-import BbsList from "../bbs/BbsList"
-import BbsWrite from "../bbs/BbsWrite"
-import BbsDetail from "../bbs/BbsDetail"
-import BbsUpdate from "../bbs/BbsUpdate"
-import BbsAnswer from "../bbs/BbsAnswer"
+import Cart from "../cart/Cart"
+import Cart_Detail from "../cart/Cart_Detail"
+import Admin from "../admin/Admin"
+import Admin_aside from "../admin/Admin_aside"
+import Store from "../store/Store"
+import Store_Detail from "../store/Store_Detail"
+import Payment from "../payment/Payment"
+import Payment_Detail from "../payment/Payment_Detail"
+import Mypage from "../member/Mypage"
 import Join from "../member/Join"
 import Login from "../member/Login"
 import Logout from "../member/Logout"
@@ -17,15 +21,19 @@ function Router() {
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 
-				<Route path="/bbslist" element={<BbsList />}></Route>
-				<Route path="/bbswrite" element={<BbsWrite />}></Route>
-				<Route path="/bbsdetail/:seq" element={<BbsDetail />}></Route>
-				<Route path="/bbsupdate" element={<BbsUpdate />}></Route>
-				<Route path="/bbsanswer/:parentSeq" element={<BbsAnswer />}></Route>
-
+				<Route path="/cart" element={<Cart />}></Route>
+				<Route path="/cart_detail/:cartp_seq" element={<Cart_Detail />}></Route>
+				<Route path="/admin/" element={<Admin />}></Route>
+				<Route path="/admin_aside" element={<Admin_aside />}></Route>
+				<Route path="/store/" element={<Store />}></Route>
+				<Route path="/store_detail/:prod_seq" element={<Store_Detail />}></Route>
+				<Route path="/payment/" element={<Payment />}></Route>
+				<Route path="/payment_detail/" element={<Payment_Detail />}></Route>
 				<Route path="/login" element={<Login />}></Route>
 				<Route path="/join" element={<Join />}></Route>
 				<Route path="/logout" element={<Logout />}></Route>
+				<Route path="/mypage" element={<Mypage />}></Route>
+
 			</Routes>
 	);
 }
