267083f97a22d45128a930f1be5b2f73fc995653
jiwoon je <wpwldens@gmail.com>
2024-04-17 12:42:31 +0900

    FE 매핑 수정

--- Apple_Front/src/member/Mypage.js
+++ Apple_Front/src/member/Mypage.js
@@ -3,3 +3,16 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthProvider";
 import { HttpHeadersContext } from "../context/HttpHeadersProvider";
+
+function Mypage() {
+
+
+    return (
+		<div>
+			<h2> Mypage 입니다. </h2>
+		</div>
+	);
+    
+    }
+    
+    export default Mypage;
