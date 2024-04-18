b15e6627805879cbe26c01fbed8f2058d19eeccf
aa <a@a>
2024-04-18 18:09:03 +0900

    Spring Config & FE - Context에 주석 추가

--- Apple_Back/src/main/java/com/example/phoneShopping/config/DatabaseConfig.java
+++ Apple_Back/src/main/java/com/example/phoneShopping/config/DatabaseConfig.java
@@ -1,45 +1,42 @@
 package com.example.phoneShopping.config;
 
-// 자바의 로깅(Logging) 라이브러리 조합 = SLF4J+Logback
-// slf4j(Simple Logging Facade for Java) = 로깅 Facade(퍼사드) = 로깅에 대한 추상 레이어를 제공하는 interface의 모음 / Logback를 사용하기 쉽게 포장하는 역할
-// Logback = 실질적인 로깅 라이브러리 구현체
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
+import org.slf4j.LoggerFactory; // Log남기기 위한 설정 1
+import org.slf4j.Logger;		// Log남기기 위한 설정 2
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConfigurationProperties; // 외부 파일 가져와서 사용하는 것 허용
+import org.springframework.context.annotation.Bean; // Bean(SW component)사용
+import org.springframework.context.annotation.Configuration; // 설정 파일임을 명시
+import org.springframework.context.annotation.PropertySource; // 스프링 환경설정파일 위치 명시
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariConfig; // DB연결 도와주는 라이브러리 1
+import com.zaxxer.hikari.HikariDataSource; // DB연결 도와주는 라이브러리 2
 import java.util.Date;
-import javax.sql.DataSource;
+import javax.sql.DataSource; // DB연결 도와주는 라이브러리 3
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
+import org.apache.ibatis.session.SqlSessionFactory; // MyBatis 사용하기 위한 설정 1
+import org.mybatis.spring.SqlSessionFactoryBean;	// MyBatis 사용하기 위한 설정 2
+import org.mybatis.spring.SqlSessionTemplate;		// MyBatis 사용하기 위한 설정 3
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.Resource;		// 사용 자원에 대한 설명 명시
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver; // 사용한 자원의 위치 명시
 
 @Configuration
 @PropertySource("classpath:application.properties")
 public class DatabaseConfig 
 {
     private final Logger logger = LoggerFactory.getLogger(
-            DatabaseConfig.class);
+            DatabaseConfig.class);					// DatabaseConfig.class에 대한 로그 파일을 남기기 위해 선언
 
         @Bean
         @ConfigurationProperties(prefix = "spring.datasource.hikari")
-        public HikariConfig hikariConfig() 
+        public HikariConfig hikariConfig() // DB와 연동하여 사용하기 위해 Hikari Config 구성
         {
             logger.info("DatabaseConfig dataSource(): {}", new Date());
             return new HikariConfig();
         }
 
         @Bean
-        public DataSource dataSource() 
+        public DataSource dataSource() // DB와 연동하여 사용하기 위해 Data Source 구성
         {
             logger.info("DatabaseConfig dataSource(): {}", new Date());
             DataSource dataSource = new HikariDataSource(hikariConfig());
@@ -48,7 +45,7 @@ public class DatabaseConfig
         }
 
         @Bean
-        public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception 
+        public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception // 위에서 구성한 환경들을 기반으로 MyBatis에서 SQL처리 설정 1
         {
             logger.info("DatabaseConfig sqlSessionFactory(): {}", new Date());
             SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
@@ -62,7 +59,7 @@ public class DatabaseConfig
         }
 
         @Bean
-        public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) 
+        public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory)  // MyBatis에서 SQL처리 설정 2
         {
             return new SqlSessionTemplate(sqlSessionFactory);
         }
--- Apple_Back/src/main/java/com/example/phoneShopping/security/UserDetailsServiceImpl.java
+++ Apple_Back/src/main/java/com/example/phoneShopping/security/UserDetailsServiceImpl.java
@@ -1,11 +1,11 @@
 package com.example.phoneShopping.security;
 
 import java.util.Date;
-import com.example.phoneShopping.member.dao.MemberDao;
-import com.example.phoneShopping.member.domain.Member;
+import com.example.phoneShopping.member.dao.MemberDao;	// MemberDao 사용
+import com.example.phoneShopping.member.domain.Member;	// Member 사용
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;	// username(id) 없을 경우 오류 안내해주는 class
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +13,14 @@ public class UserDetailsServiceImpl implements UserDetailsService
 {
     private final MemberDao memberDao;
 
-    public UserDetailsServiceImpl(MemberDao memberDao) 
+    public UserDetailsServiceImpl(MemberDao memberDao) 	// 기본 생성자
     {
         this.memberDao = memberDao;
     }
 
     // username = User (id)
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException 
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException // 사용자 정보 불러오기
     {
         System.out.println("UserDetailsServiceImpl loadUserByUsername " + new Date());
 
