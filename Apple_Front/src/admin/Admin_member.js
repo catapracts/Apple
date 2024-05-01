02217df8039cfcbe491cb24e871636c5f9228d9f
jiwoon je <wpwldens@gmail.com>
2024-05-01 18:25:12 +0900

    240501 FE mypage connecting

--- Apple_Front/src/admin/Admin_member.js
+++ Apple_Front/src/admin/Admin_member.js
@@ -1,10 +1,36 @@
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect} from "react";
+
 import Button from '../common/Button';
 import './Admin.css';
 
 function Admin_member() {
+
+    const [memberList, setMemberList] = useState([]);
+    const [memId, setMemId] = useState("");
     
     const navigate = useNavigate();
+
+    /* 회원 전체 조회 */
+	const getAll = async () => {
+
+		await axios.get("http://localhost:3000/user/getAll")
+			.then((resp) => {											// 회원가입 성공 시 출력
+				console.log("[TestFindAll.js] getAll() success :D");
+				
+                console.log(resp.data);
+                setMemberList(resp.data);
+
+			}).catch((err) => {											// 회원가입 실패 시 출력
+				console.log("[TestFindAll.js] getAll() error :<");
+				console.log(err);
+			});
+	}
+
+    useEffect(() => {
+		getAll();
+	}, []);
 
     return (
         <div>
@@ -18,14 +44,16 @@ function Admin_member() {
                             <thead>
                                 <th className="member_th">회원번호</th>
                                 <th className="member_th">회원 ID</th>
-                                <th className="member_th">회원이름</th>
+                                <th className="member_th">회원 PW</th>
                             </thead>
                             <tbody>
-                                <td className="member_td">
-                                    <Link to="/admin/member/1">내용</Link>
-                                </td>
-                                <td className="member_td">내용</td>
-                                <td className="member_td">내용</td>
+                                { 
+                                    memberList.map( function(member, idx) {
+                                        return(
+                                            <TableRow obj={member} key={idx} cnt={idx + 1} />
+                                        )
+                                    })
+                                }
                             </tbody>
                         </table>
                     </div>
@@ -37,6 +65,29 @@ function Admin_member() {
             </div>
         </div>
     );
+}
+
+function TableRow(props) {
+	const member = props.obj;
+
+	return (
+			<tr>
+					{
+						<>
+							<td className="member_td">
+								{member.memSeq}
+							</td>
+
+                            <td className="member_td">
+                                <Link to={{ pathname: `/admin/member/${member.memId}` }}>
+                                <span className="underline bbs-title" >{member.memId}</span>
+                                </Link>
+                            </td>
+                            <td className="member_td">{member.memPw}</td>
+						</>
+					}			
+			</tr>
+	);
 }
 
 export default Admin_member;
