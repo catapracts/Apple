3dfa2286b68c50a6f9d99756366bd80dfb893fe1
jiwoon je <wpwldens@gmail.com>
2024-05-03 13:39:18 +0900

    240503 scroll, logo edit

--- Apple_Front/src/App.js
+++ Apple_Front/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./common/Header"
 import Main from "./common/Main"
 import Footer from "./common/Footer"
+import ScrollToTop from "./context/ScrollToTop";
 import AuthProvider from "./context/AuthProvider"
 import HttpHeadersProvider from "./context/HttpHeadersProvider";  // 여기까지가 페이지에 구성할 Component들 불러오기
 
@@ -16,7 +17,9 @@ function App() {
         <Header />     
         <AuthProvider>              {/* 회원 정보 인증(Id, pw) */}
           <HttpHeadersProvider>     {/* 회원 정보 인증(JWT) */}
-            <Main />                {/* 화면에 띄울 페이지 */}
+            <ScrollToTop>           {/* 화면 바뀔 때 스크롤을 최 상단으로 */}
+              <Main />                {/* 화면에 띄울 페이지 */}
+            </ScrollToTop>
           </HttpHeadersProvider>
         </AuthProvider>
         <Footer />
