98a3613854f5a4301a86d7ab599d101520a01557
jiwoon je <wpwldens@gmail.com>
2024-05-07 10:36:07 +0900

    240507 FE remove test file

--- Apple_Front/src/router/Router.js
+++ Apple_Front/src/router/Router.js
@@ -1,35 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../common/Home"
+
 import Cart from "../cart/Cart"
-import TestCartFindAll from "../cart/TestCartFindAll";
-import TestCartCreate from "../cart/TestCartCreate";
-import TestCartGetOne from "../cart/TestCartGetOne";
-import TestCartUpdate from "../cart/TestCartUpdate";
-
-import TestCartProductFindAll from "../cart/TestCartProductFindAll";
-import TestCartProductCreate from "../cart/TestCartProductCreate";
-import TestCartProductGetOne from "../cart/TestCartProductGetOne";
-import TestCartProductUpdate from "../cart/TestCartProductUpdate";
-
 
 import Admin_product from "../admin/Admin_product"
 import Admin_product_detail from "../admin/Admin_product_detail"
 import Admin_product_create from "../admin/Admin_product_create"
 import Admin_product_update from "../admin/Admin_product_update"
+
 import Admin_hdd_detail from "../admin/Admin_hdd_detail"
 import Admin_hdd_update from "../admin/Admin_hdd_update"
+
 import Admin_color_detail from "../admin/Admin_color_detail"
 import Admin_color_update from "../admin/Admin_color_update"
+
 import Admin_plus_detail from "../admin/Admin_plus_detail"
 import Admin_plus_update from "../admin/Admin_plus_update"
 
 import Admin_payment from "../admin/Admin_payment"
 import Admin_payment_detail from "../admin/Admin_payment_detail"
+
 import Admin_member from "../admin/Admin_member"
 import Admin_member_detail from "../admin/Admin_member_detail"
-import Store from "../store/Store"
 
+import Store from "../store/Store"
 import Store_Detail_1 from "../store/Store_Detail_1"
 import Store_Detail_2 from "../store/Store_Detail_2"
 import Store_Detail_3 from "../store/Store_Detail_3"
@@ -46,31 +41,8 @@ import Mypage_payment_detail from "../member/Mypage_payment_detail"
 import Join from "../member/Join"
 import Login from "../member/Login"
 import Logout from "../member/Logout"
-import TestFindAll from "../member/TestFindAll"
-import TestgetOne from "../member/TestgetOne"
-import TestUpdate from "../member/TestUpdate"
-
-
-import TestAddrFindAll from "../member/TestAddrFindAll"
-import TestAddrCreate from "../member/TestAddrCreate"
-import TestAddrGetOne from "../member/TestAddrGetOne"
-import TestAddrUpdate from "../member/TestAddrUpdate"
-
-
-import TestInfoFindAll from "../member/TestInfoFindAll"
-import TestInfoCreate from "../member/TestInfoCreate"
-import TestInfoGetOne from "../member/TestInfoGetOne"
-import TestInfoUpdate from "../member/TestInfoUpdate"
-
-
-import TestCardFindAll from "../member/TestCardFindAll"
-import TestCardCreate from "../member/TestCardCreate"
-import TestCardGetOne from "../member/TestCardGetOne"
-import TestCardUpdate from "../member/TestCardUpdate"
-
 
 import Product from "../product/Product"
-
 import Product_Detail_1 from "../product/Product_Detail_1"
 import Product_Detail_2 from "../product/Product_Detail_2"
 import Product_Detail_3 from "../product/Product_Detail_3"
@@ -84,24 +56,18 @@ function Router() {
 				<Route path="/" element={<Home />}></Route>
 
 				<Route path="/cart" element={<Cart />}></Route>
-				<Route path="/TestCartFindAll" element={<TestCartFindAll />}></Route>
-				<Route path="/TestCartCreate" element={<TestCartCreate />}></Route>
-				<Route path="/TestCartGetOne/:cartSeq" element={<TestCartGetOne />}></Route>
-				<Route path="/TestCartUpdate/:cartSeq" element={<TestCartUpdate />}></Route>
-
-				<Route path="/TestCartProductFindAll" element={<TestCartProductFindAll />}></Route>
-				<Route path="/TestCartProductCreate" element={<TestCartProductCreate />}></Route>
-				<Route path="/TestCartProductGetOne/:cartpSeq" element={<TestCartProductGetOne />}></Route>
-				<Route path="/TestCartProductUpdate/:cartpSeq" element={<TestCartProductUpdate />}></Route>
 
 				<Route path="/admin/product" element={<Admin_product />}></Route>
 				<Route path="/admin/product/create" element={<Admin_product_create />}></Route>
 				<Route path="/admin/product/getOne/:prodSeq" element={<Admin_product_detail />}></Route>
 				<Route path="/admin/product/update/:prodSeq" element={<Admin_product_update />}></Route>
+
 				<Route path="/admin/hdd/getOne/:hddSeq" element={<Admin_hdd_detail />}></Route>
 				<Route path="/admin/hdd/update/:hddSeq" element={<Admin_hdd_update />}></Route>
+
 				<Route path="/admin/color/getOne/:colorSeq" element={<Admin_color_detail />}></Route>
 				<Route path="/admin/color/update/:colorSeq" element={<Admin_color_update />}></Route>
+
 				<Route path="/admin/plus/getOne/:plusSeq" element={<Admin_plus_detail />}></Route>
 				<Route path="/admin/plus/update/:plusSeq" element={<Admin_plus_update />}></Route>
 
@@ -109,58 +75,28 @@ function Router() {
 				<Route path="/payment/:paySeq" element={<Admin_payment_detail />}></Route>
 				<Route path="/admin/member" element={<Admin_member />}></Route>
 				<Route path="/admin/member/:memId" element={<Admin_member_detail />}></Route>
+
 				<Route path="/store/" element={<Store />}></Route>
 				<Route path="/store_detail/1" element={<Store_Detail_1 />}></Route>
 				<Route path="/store_detail/2" element={<Store_Detail_2 />}></Route>
 				<Route path="/store_detail/3" element={<Store_Detail_3 />}></Route>
 
 				<Route path="/payment/" element={<Payment />}></Route>
-				<Route path="/payment_backup" element={<Payment_backup />}></Route>
 				<Route path="/payment_success" element={<Payment_success />}></Route>
-
 
 				<Route path="/mypage/:memId" element={<Mypage />}></Route>
 				<Route path="/mypage/update/2" element={<Mypage_update />}></Route>
 				<Route path="/mypage/payment" element={<Mypage_payment />}></Route>
 				<Route path="/mypage/payment/:paySeq" element={<Mypage_payment_detail />}></Route>
+
 				<Route path="/product" element={<Product />}></Route>
-
-
 				<Route path="/product_detail/1" element={<Product_Detail_1 />}></Route>
 				<Route path="/product_detail/2" element={<Product_Detail_2 />}></Route>
 				<Route path="/product_detail/3" element={<Product_Detail_3 />}></Route>
 
-
-				<Route path="/login" element={<Login />}></Route>
 				<Route path="/join" element={<Join />}></Route>
+				<Route path="/login" element={<Login />}></Route>
 				<Route path="/logout" element={<Logout />}></Route>
-				<Route path="/TestFindAll" element={<TestFindAll />}></Route>
-				<Route path="/TestgetOne/:memId" element={<TestgetOne />}></Route>
-				<Route path="/TestUpdate/:memId" element={<TestUpdate />}></Route>
-
-
-				<Route path="/TestFindAll" element={<TestFindAll />}></Route>
-				<Route path="/TestgetOne/:memId" element={<TestgetOne />}></Route>
-				<Route path="/TestUpdate/:memId" element={<TestUpdate />}></Route>
-
-
-				<Route path="/TestAddrFindAll" element={<TestAddrFindAll />}></Route>
-				<Route path="/TestAddrCreate" element={<TestAddrCreate />}></Route>
-				<Route path="/TestAddrGetOne/:addrSeq" element={<TestAddrGetOne />}></Route>
-				<Route path="/TestAddrUpdate/:addrSeq" element={<TestAddrUpdate />}></Route>
-
-
-				<Route path="/TestInfoFindAll" element={<TestInfoFindAll />}></Route>
-				<Route path="/TestInfoCreate" element={<TestInfoCreate />}></Route>
-				<Route path="/TestInfoGetOne/:infoSeq" element={<TestInfoGetOne />}></Route>
-				<Route path="/TestInfoUpdate/:infoSeq" element={<TestInfoUpdate />}></Route>
-
-
-				<Route path="/TestCardFindAll" element={<TestCardFindAll />}></Route>
-				<Route path="/TestCardCreate" element={<TestCardCreate />}></Route>
-				<Route path="/TestCardGetOne/:cardSeq" element={<TestCardGetOne />}></Route>
-				<Route path="/TestCardUpdate/:cardSeq" element={<TestCardUpdate />}></Route>
-
 			</Routes>
 	);
 }
